8efa150f8199cc21418e10feda4266f58e6ec5d3
Leonid Vysochyn <leonate@gmail.com>
2017-10-28 01:19:58 +0200

    initial commit

---
+++ src/stuckinaloop/StuckInALoop.kt
@@ -0,0 +1,10 @@
+package stuckinaloop
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    with(Scanner(System.`in`)){
+        val n = nextInt()
+        (1..n).forEach{ println("$it Abracadabra") }
+    }
+}
